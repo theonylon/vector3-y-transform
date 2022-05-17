f7c8318c51b0b88d4523a5513be6a61e92dbdf81
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-05-17 10:21:49 -0300

    exercise part 1 done

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 movementSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position += MovementSpeed;
+
+        transform.eulerAngles += movementSpeed;
+    }
+}
---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+    public Vector3 miVector;
+    public GameObject myObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myObject.transform.position = miVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
